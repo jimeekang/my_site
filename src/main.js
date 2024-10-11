5019131c70978c66c7b92642cd9dd9fee8eadc5d
Jimee <86811452+jimeekang@users.noreply.github.com>
2024-10-11 12:11:58 +1100

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
 // Header에 페이지 아래로 스크로시 다크 스타일링 적용
 const header = document.querySelector(".header");
 let headerHeight = header.getBoundingClientRect().height;
-
 document.addEventListener("scroll", () => {
-  let currentScroll = window.scrollY.toFixed(0);
+  let currentScroll = window.scrollY;
   if (currentScroll > headerHeight) {
     header.classList.add("header--dark");
   } else {
     header.classList.remove("header--dark");
   }
+});
 
-  /*
-  if (currentScroll > headerHeight) {
-    header.style.backgroundColor = "#1b1e26";
-    header.style.transition = "all 250ms";
-  } else {
-    header.style.backgroundColor = "#050a13";
-  }
-  */
+// home 섹션을 아래로 스크롤시 투명하게 만들기
+const home = document.querySelector(".home__container");
+const homeHeight = home.offsetHeight;
+document.addEventListener("scroll", () => {
+  let currentScroll = window.scrollY;
+  home.style.opacity = 1 - currentScroll / homeHeight;
 });
